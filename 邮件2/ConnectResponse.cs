4e32770c643e85aad18e0d7b951beee01d2c9088
GuoBingBu <964404666@qq.com>
2020-02-03 14:32:27 +0800

    提交信息

---
+++ 邮件2/ConnectResponse.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+
+/// 编 码 人：BGB
+
+
+/// </summary>
+using System;
+using System.IO;
+
+namespace DotNet.Utilities
+{
+    internal sealed class ConnectResponse : Pop3Response
+    {
+        private Stream _networkStream;
+        public Stream NetworkStream
+        {
+            get
+            {
+                return _networkStream;
+            }
+        }
+
+        public ConnectResponse(Pop3Response response, Stream networkStream)
+            : base(response.ResponseContents, response.HostMessage, response.StatusIndicator)
+        {
+            if (networkStream == null)
+            {
+                throw new ArgumentNullException("networkStream");
+            }
+            _networkStream = networkStream;
+        }
+    }
+}
---
+++ 邮件2/QuitCommand.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+
+/// 编 码 人：BGB
+
+
+/// </summary>
+using System.IO;
+
+namespace DotNet.Utilities
+{
+    /// <summary>
+    /// This class represents the Pop3 QUIT command.
+    /// </summary>
+    internal sealed class QuitCommand : Pop3Command<Pop3Response>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuitCommand"/> class.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        public QuitCommand(Stream stream)
+            : base(stream, false, Pop3State.Transaction | Pop3State.Authorization) { }
+
+        /// <summary>
+        /// Creates the Quit request message.
+        /// </summary>
+        /// <returns>
+        /// The byte[] containing the QUIT request message.
+        /// </returns>
+        protected override byte[] CreateRequestMessage()
+        {
+            return GetRequestMessage(Pop3Commands.Quit);
+        }
+    }
+}
